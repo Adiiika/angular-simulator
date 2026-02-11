8d1c203e59dfa7418650e414fc63c717cbbbd3f6
Adam <adamsagidov1@gmail.com>
2026-02-11 13:18:53 +0300

    did all tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import './training.ts';
+import { Color } from '../enums/Color.js';
+import './collection.js'
 
 @Component({
   selector: 'app-root',
@@ -8,5 +10,36 @@ import './training.ts';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  companyName: string = 'РУМТИБЕТ';
 
+  constructor() {
+    this.lastVisit();
+    this.countLogin();
+  }
+
+  isPrimaryColor(color: Color): boolean {
+    const primaryColor: Color[] = [Color.Red, Color.Green, Color.Blue];
+
+    return primaryColor.includes(color);
+  }
+
+  lastVisit() {
+    let lastLogin: string = new Date().toString();
+    localStorage.getItem('lastVisit');
+
+    if (lastLogin) {
+      localStorage.setItem('lastVisit', lastLogin);
+      console.log('Последний вход', lastLogin);
+    }
+  }
+
+  countLogin() {
+    let visitsStored: number = Number(localStorage.getItem('Visits') || '0');
+
+    if (visitsStored) {
+      visitsStored += 1;
+      localStorage.setItem('Visits', visitsStored.toString());
+      console.log('Количество заходов', visitsStored);
+    }
+  }
 }
