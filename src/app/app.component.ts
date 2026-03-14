c0daa9f3d3a641cc73714aca3154ed435048d97f
Adam <adamsagidov1@gmail.com>
2026-03-14 12:05:09 +0300

    fix errors v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,18 @@
 import { Component, inject } from '@angular/core';
-import './training.ts';
-import { Color } from '../enums/Color.js';
-import './collection.js';
+import { FormsModule, NgModel } from '@angular/forms';
+import { CommonModule, NgComponentOutlet, NgTemplateOutlet } from '@angular/common';
+
+import { MessageService } from './services/message.service.js';
+import { LocalStorageService } from './services/local-storage.service.js';
 import { IOffer } from '../interfaces/IOffer.js';
 import { IPath } from '../interfaces/IPaths.js';
-import { FormsModule, NgModel } from '@angular/forms';
 import { IBlog } from '../interfaces/IBlog.js';
 import { IMessages } from '../interfaces/IMessage.js';
+import { ISearchQuery } from '../interfaces/ISearchQuery.js';
+import './training.ts';
+import './collection.js';
+import { Color } from '../enums/Color.js';
 import { MessageType } from '../enums/MessageType.js';
-import { MessageService } from './message.service.js';
-import { CommonModule, NgComponentOutlet, NgTemplateOutlet } from '@angular/common';
-import { LocalStorageService } from './local-storage.service.js';
 
 @Component({
   selector: 'app-root',
@@ -23,19 +25,20 @@ export class AppComponent {
   messageService: MessageService = inject(MessageService);
   localStorageService: LocalStorageService = inject(LocalStorageService);
 
+  msg = MessageType;
   companyName: string = 'РУМТИБЕТ';
-
   selectedOfferId: number | null = null;
-
   date: string = '';
-
   counter: number = 0;
-
   loadPage: boolean = true;
-
   isClickerMode: boolean = true;
-
   userText: string = '';
+
+  searchQuery: ISearchQuery = {
+    townName: '',
+    tourDate: '',
+    humanCount: '',
+  }
 
   offers: IOffer[] = [
     {
@@ -78,7 +81,7 @@ export class AppComponent {
     {
       id: 3,
       rating: '5.0',
-      image: 'purple-sky',
+      image: 'starry-sky',
       title: 'Растяжка в горах',
       subtitle: 'для тех, кто забоится о себе',
       price: '230 $'
@@ -88,39 +91,33 @@ export class AppComponent {
   blogs: IBlog[] = [
     {
       id: 1,
-      image: 'italia',
+      image: 'italy-city',
       title: 'Красивая Италия, какая она в реальности?',
       description: 'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации.',
       date: '01/04/2023'
     },
     {
       id: 2,
-      image: 'clouds',
+      image: 'plane-view',
       title: 'Долой сомнения! Весь мир открыт для вас!',
       description: 'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации ... независимые способы реализации соответствующих...',
       date: '01/04/2023'
     },
     {
       id: 3,
-      image: 'street',
+      image: 'person-between-buildings',
       title: 'Как подготовиться к путешествию в одиночку?',
       description: 'Для современного мира базовый вектор развития предполагает.',
       date: '01/04/2023'
     },
     {
       id: 4,
-      image: 'india',
+      image: 'india-mosque',
       title: 'Индия ... летим?',
       description: 'Для современного мира базовый.',
       date: '01/04/2023'
     }
   ]
-
-  searchQuery = {
-    townName: '',
-    tourDate: '',
-    humanCount: '',
-  }
 
   constructor() {
     this.lastVisit();
---
+++ src/app/services/local-storage.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class LocalStorageService {
+
+  setItem<T>(key: string, value: T): void {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  getItem<T>(key: string): T | null {
+    const data = localStorage.getItem(key)
+    if (!data) return null;
+
+    try {
+      return JSON.parse(data);
+    }
+    catch (error) {
+      console.error(`Error parsing storage key "${key}":`, error);
+      return null;
+    }
+  }
+
+  removeItem(value: string): void {
+    localStorage.removeItem(value);
+  }
+
+  clearAll(): void {
+    localStorage.clear();
+  }
+
+}
