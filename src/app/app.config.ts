40ced29419bf25bb09eb32804432c2922369c9e2
Adam <adamsagidov1@gmail.com>
2026-06-19 10:23:07 +0300

    did all tasks

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
@@ -8,6 +9,8 @@ import Nora from '@primeuix/themes/nora';
 import Lara from '@primeuix/themes/lara';
 import { Theme } from '../enums/Theme';
 import { Preset } from '@primeuix/themes/types';
+import { requestInterceptor } from './request.interceptor';
+import { errorHandlingInterceptor } from './error-handling.interceptor';
 
 const initTheme = (): Preset => {
   const themeFromStorage: Theme | null = localStorage.getItem('theme') as Theme;
@@ -21,7 +24,9 @@ const initTheme = (): Preset => {
 }
 
 export const appConfig: ApplicationConfig = {
+
   providers: [
+    provideHttpClient(withInterceptors([requestInterceptor, errorHandlingInterceptor])),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideAnimationsAsync(),
@@ -35,4 +40,5 @@ export const appConfig: ApplicationConfig = {
       }
     })
   ]
+
 };
---
+++ src/app/request.interceptor.ts
@@ -0,0 +1,19 @@
+import { HttpErrorResponse, HttpEvent, HttpEventType, HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
+import { catchError, tap, throwError } from 'rxjs';
+
+export const requestInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next: HttpHandlerFn) => {
+
+  const requestTime: number = Date.now();
+
+  return next(req).pipe(
+    tap((event: HttpEvent<unknown>) => {
+      if (event.type === HttpEventType.Response) {
+        console.log(req);
+        console.log(req.method, req.url, event.status, Date.now() - requestTime);
+      }
+    }), catchError((error: HttpErrorResponse) => {
+      console.log(`Ошибка ${error.status}`);
+      return throwError(() => error);
+    })
+  );
+}
