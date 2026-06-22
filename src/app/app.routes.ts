fdaf63bdb38124240bcac6e4e8fd48ea31019ad1
Adam <adamsagidov1@gmail.com>
2026-06-22 08:54:20 +0300

    commited errors

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,9 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 
 export const routes: Routes = [
+
     {
         path: '',
-        component: HomePageComponent,
         loadComponent: () => import('./home-page/home-page.component').then((m) => m.HomePageComponent),
     },
     {
@@ -15,4 +15,5 @@ export const routes: Routes = [
         path: '**',
         loadComponent: () => import('./not-found-page/not-found-page.component').then((m) => m.NotFoundPageComponent),
     },
+
 ];
